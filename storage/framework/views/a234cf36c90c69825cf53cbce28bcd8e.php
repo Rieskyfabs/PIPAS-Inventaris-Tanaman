a7c8e327aa643f7fce4daa6569b2fffdbcd8b32a
DaffyFauzan <daffi.fauzan@gmail.com>
2024-10-17 08:44:08 +0700

    Updated the Readme.MD

--- storage/framework/views/a234cf36c90c69825cf53cbce28bcd8e.php
+++ storage/framework/views/a234cf36c90c69825cf53cbce28bcd8e.php
@@ -45,14 +45,14 @@
                         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                             <?php if(auth()->guard()->check()): ?>
                                 <?php if(Auth::user()->role->name == 'admin'): ?>
-                                    <a href="<?php echo e(route('admin/dashboard')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> Admin Dashboard</a>
+                                    <a href="<?php echo e(route('admin/dashboard')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> <?php echo e(__('Admin Dashboard')); ?></a>
                                 <?php elseif(Auth::user()->role->name == 'master'): ?>
-                                    <a href="<?php echo e(route('master/dashboard')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> Master Dashboard</a>
+                                    <a href="<?php echo e(route('master/dashboard')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> <?php echo e(__('Master Dashboard')); ?></a>
                                 <?php else: ?>
-                                    <a href="<?php echo e(route('dashboard')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> User Dashboard</a>
+                                    <a href="<?php echo e(route('dashboard')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> <?php echo e(__('User Dashboard')); ?></a>
                                 <?php endif; ?>
                             <?php else: ?>
-                                <a href="<?php echo e(route('login')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> Login</a>
+                                <a href="<?php echo e(route('login')); ?>" class="btn-login"><i class="fas fa-sign-in-alt"></i> <?php echo e(__('Login')); ?></a>
                             <?php endif; ?>
                         </div>
                     <?php endif; ?>
@@ -75,16 +75,19 @@
                                 <?php if(auth()->guard()->check()): ?>
                                     <?php if(Auth::user()->role->name == 'admin'): ?> 
                                         <a href="<?php echo e(route('admin/dashboard')); ?>" class="btn-login">
-                                            <i class="fas fa-sign-in-alt"></i> Admin Dashboard
+                                            <i class="fas fa-sign-in-alt"></i> <?php echo e(__('Admin Dashboard')); ?>
+
                                         </a>
                                     <?php else: ?>
                                         <a href="<?php echo e(route('dashboard')); ?>" class="btn-login">
-                                            <i class="fas fa-sign-in-alt"></i> User Dashboard
+                                            <i class="fas fa-sign-in-alt"></i> <?php echo e(__('User Dashboard')); ?>
+
                                         </a>
                                     <?php endif; ?>
                                 <?php else: ?>
                                     <a href="<?php echo e(route('login')); ?>" class="btn-login">
-                                        <i class="fas fa-sign-in-alt"></i> Login
+                                        <i class="fas fa-sign-in-alt"></i> <?php echo e(__('Login')); ?>
+
                                     </a>
                                 <?php endif; ?>
                             <?php endif; ?>  
