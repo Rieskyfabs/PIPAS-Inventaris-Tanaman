1f29c7f565a5adb4c29a7538150ed65be6a96ff5
DaffyFauzan <daffi.fauzan@gmail.com>
2025-01-15 21:45:10 +0700

    changed the SIM PIPAS to SIM PANTAU TANAMAN

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,14 @@
 <!-- ======= Header ======= -->
 <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <x-logo-sidebar 
-        logoSrc="{{ asset('images/wikrama-logo.png')}} " 
-        logoText="SIM PIPAS" 
+    <x-logo-sidebar
+        logoSrc="{{ asset('images/wikrama-logo.png')}} "
+        logoText="Pantau Tanaman"
     />
     <!-- End Logo -->
 
-    {{-- <x-sidebar-search 
-        actionUrl="#" 
+    {{-- <x-sidebar-search
+        actionUrl="#"
     /> --}}
     <!-- End Search Bar -->
 
@@ -22,18 +22,18 @@
             </li>
             <!-- End Search Icon -->
 
-            <x-notification-dropdown 
-                :notificationCount="$notificationCount" 
-                :notifications="$notifications" 
+            <x-notification-dropdown
+                :notificationCount="$notificationCount"
+                :notifications="$notifications"
             />
-        
-            <x-profile-dropdown 
-                profileImage="{{ Auth::user()->profile_image ?? Avatar::create(Auth::user()->username)->toBase64()  }}" 
-                username="{{ Auth::user()->username }}" 
+
+            <x-profile-dropdown
+                profileImage="{{ Auth::user()->profile_image ?? Avatar::create(Auth::user()->username)->toBase64()  }}"
+                username="{{ Auth::user()->username }}"
                 email="{{ Auth::user()->email }}"
                 role="{{ Auth::user()->role->name }}"
-                profileUrl="#" 
-                helpUrl="#" 
+                profileUrl="#"
+                helpUrl="#"
             />
 
         </ul>
